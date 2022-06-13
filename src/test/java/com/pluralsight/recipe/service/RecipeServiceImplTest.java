0de06218871019a6e488ffbf8645791394496329
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-13 15:18:15 +0530

    Use Java 11

--- src/test/java/com/pluralsight/recipe/service/RecipeServiceImplTest.java
+++ src/test/java/com/pluralsight/recipe/service/RecipeServiceImplTest.java
@@ -37,7 +37,6 @@ class RecipeServiceImplTest {
         ArrayList<Recipe> list =new ArrayList<>(); //Creation of ArrayList
         list.addAll(recipeData); //HashSet to ArrayList
 
-
         when(repository.findAll()).thenReturn(list);
 
         Set<Recipe> recipes_r =  recipeService.getRecipes();
