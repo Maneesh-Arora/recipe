bc6834adbd0f019f785a8c473cd5cbc64a820f0b
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-14 14:53:55 +0530

    Added Recipe Form to add a new recipe and update the controller accordingly

--- src/main/java/com/pluralsight/recipe/controllers/RecipeController.java
+++ src/main/java/com/pluralsight/recipe/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.pluralsight.recipe.controllers;
 
+import com.pluralsight.recipe.commands.RecipeCommand;
 import com.pluralsight.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,4 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe")
+    public String saveUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand){
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
